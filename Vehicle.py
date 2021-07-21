87f899ee02e60ddc1b33cb9677f003a7f31456ea
kiwi-fruitiwi <zane.tian@gmail.com>
2021-07-21 13:51:56 -0700

    Add pursue, evade. Move check_edges to Vehicle

--- Vehicle.py
+++ Vehicle.py
@@ -7,11 +7,13 @@ class Vehicle:
         self.pos = PVector(x, y)
         self.vel = PVector(random(-1, 1), random(-1, 1))
         self.acc = PVector(0, 0)
-        self.max_speed = random(4, 7)
-        self.max_force = 0.05
+        self.max_speed = random(3, 5)
+        self.max_force = 0.2
         self.r = 10
     
     
+    # this is the inverse of seek; you literally multiply the seek function's returned
+    # force vector by -1
     def flee(self, target):
         return self.seek(target).mult(-1)
         
@@ -32,6 +34,28 @@ class Vehicle:
         
         # apply force! be careful of force vs acc
         return desired
+    
+    
+    def pursue(self, target): 
+        # target is not a position vector, but a vehicle 
+        # because we need to know its velocity
+        target_pos = target.pos.copy()
+        
+        # we have to copy position and velocity because we don't want to mutate the original copy
+        target_prediction = target.vel.copy()
+        
+        target_pos.add(target_prediction.mult(15))
+        
+        # fill(210, 80, 80, 50)
+        # circle(target_pos.x, target_pos.y, 16)
+        
+        return self.seek(target_pos)
+
+    
+    # evade is the inverse of pursue, where we predict where our pursuer will be and flee
+    # from that prediction
+    def evade(self, pursuer):
+        return self.pursue(pursuer).mult(-1)
         
         
     # assume self.mass is 1 so we don't need to worry about mass
@@ -65,3 +89,32 @@ class Vehicle:
         
         popMatrix()
         # draw the velocity vector
+    
+    
+    def edge_wrap(self):
+        if self.pos.x + self.r > width:
+            self.pos.x = self.r
+        if self.pos.x - self.r < 0:
+            self.pos.x = width - self.r
+        
+        if self.pos.y + self.r > height:
+            self.pos.y = self.r
+        if self.pos.y - self.r < 0:
+            self.pos.y = height - self.r
+
+
+class Target(Vehicle):
+    def __init__(self, x, y):
+        # Vehicle.__init__(self, x, y)
+        Vehicle.__init__(self, x, y)
+        self.pos = PVector(width/2, height/2-250)
+        self.max_speed = 8
+        
+        
+    def show(self):
+        # rotate the object to point where its velocity vector points        
+        pushMatrix()
+        translate(self.pos.x, self.pos.y)
+        rotate(self.vel.heading())
+        circle(0, 0, self.r*2)
+        popMatrix()        
--- sbfac.pyde
+++ sbfac.pyde
@@ -13,39 +13,45 @@ from Vehicle import *
 from random import randint
 
 def setup():
-    global vehicles, seek
+    global vehicles, target, seek
     
     colorMode(HSB, 360, 100, 100, 100)
     size(1200, 700)
     # cam = PeasyCam(this, width/2, height/2, 0, 500)
     noStroke()
     vehicles = []
+    target = Target(randint(10, width-10), randint(10, height-10))
     seek = True
     
     # create the vehicles
-    for i in range(0, 20):
+    for i in range(0, 400):
         v = Vehicle(randint(10, width-10), randint(10, height-10))
         vehicles.append(v)    
     
     
 def draw():
-    global vehicles, seek
+    global vehicles, target, seek
     
     background(210, 80, 40)    
     fill(0, 100, 80, 80)
     
     # create and display the target
     # TODO maybe make this ball a mover! - Cody
-    target_pos = PVector(mouseX, mouseY)
-    
+    # target_pos = PVector(mouseX, mouseY)
+    target_pos = target.pos
+        
     if seek:
         fill(90, 100, 100, 50)
     else:
         fill(0, 100, 100, 50)
-            
-    circle(target_pos.x, target_pos.y, 32)
     
-    # display the vehicles
+    target.show()
+    target.apply_force(PVector(150*cos(PI/180*frameCount), 150*sin(PI/180*frameCount)))
+    target.update()    
+    target.edge_wrap()
+    
+    '''
+    # display the vehicles in seek and flee mode
     for v in vehicles:
         if seek:
             v.apply_force(v.seek(target_pos))
@@ -53,6 +59,19 @@ def draw():
             v.apply_force(v.flee(target_pos))
         v.update()
         v.show()
+    '''
+    
+    # pursue and evade mode
+    for v in vehicles:
+        if seek:
+            v.apply_force(v.pursue(target))
+        else:
+            v.apply_force(v.evade(target))
+        v.update()
+        v.show()
+        v.edge_wrap()
+    
+    
 
 def mousePressed():
     global seek
